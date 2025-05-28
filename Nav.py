7f3776aa235e85cc876cae0afb2eafd6e4d9da9c
Dana Levanony <dana.levanony@gmail.com>
2025-05-28 21:00:26 +0300

    adding app

---
+++ Nav.py
@@ -0,0 +1,71 @@
+import streamlit as st
+
+import time
+import json
+from selenium import webdriver
+
+def get_nav_and_date(stock):
+    #options = webdriver.chrome.options.Options()
+    #options.add_argument("--headless")  # Run in headless mode
+    res = {'date': None, 'nav': None, 'previous close':None}
+    driver = webdriver.Chrome()
+    driver.get("https://digital.fidelity.com/prgw/digital/research/quote/dashboard/summary?symbol={}".format(stock))
+    try:
+        time.sleep(9)
+        html = driver.page_source
+        print(html)
+        if len(html.split("Nav<")) > 1:
+            date = html.split("Nav<")[1].split("As of")[1][0:7]
+            val = html.split("Nav<")[1].split("right ng-star-inserted")[1][2:].split("<")[0]
+            res['date'] = date
+            res['nav'] = val
+            print("Nav, date:{}, value:{}".format(date, val))
+
+        if res['nav'] in ['--', None]:
+            date = html.split("Previous close")[1].split("As of")[1][0:7]
+            val = html.split("Previous close")[1].split("right ng-star-inserted")[1][2:].split("<")[0]
+            res['date'] = date
+            res['previous close'] = val
+            print("Previous close, date:{}, value:{}".format(date, val))
+
+    except Exception as e:
+        print(e)
+        print("Failed")
+        res['Status'] = 'Failed'
+    driver.quit()
+    return res
+
+
+st.title(":rainbow[Hello 👋, this is our nav generator app]")
+# st.markdown(
+#     """
+#
+#     ** :rainbow[SHOW ME THE MONEY]**
+#
+#     """
+# )
+
+if st.button("Send balloons!"):
+    st.balloons()
+
+#res = get_nav_and_date('TQQQ')
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = [{"role": "assistant", "content": "Let's start chatting! 👇"}]
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("Provide SYMBOL:"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    print(prompt)
+    res = get_nav_and_date(prompt)
+    st.write(res)
